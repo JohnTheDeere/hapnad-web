020314747005e5b37d64b05858a427dc125d8dda
Martin Porkholm <martin.porkholm@outlook.com>
2025-09-20 12:48:52 +0300

    Fixed bug

--- assets/js/nav-loader.js
+++ assets/js/nav-loader.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('navbar-placeholder').innerHTML = data;
             
             // Activate the correct tab
-            const currentPage = window.location.pathname.split('/').pop();
+            let currentPage = window.location.pathname.split('/').pop();
+            // If the currentPage is empty, it means we are on the root page (index.html)
+            if (currentPage === '') {
+                currentPage = 'index.html';
+            }
+
             const navLinks = document.querySelectorAll('.nav-links a');
             navLinks.forEach(link => {
                 if (link.getAttribute('href') === currentPage) {
